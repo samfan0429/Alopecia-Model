3ae9c4ca23fb713bcbb1182adcffa61fe160b9c1
samfan0429 <spark2@macalester.edu>
2020-12-18 22:04:03 +0900

    Final Submission

--- Agent-based.py
+++ Agent-based.py
@@ -4,25 +4,25 @@ from numpy.random import rand, randint
 import matplotlib.pyplot as plt
 
 
-deltaTCell = 0.5
+# deltaTCell = 0.5
 
 # Hair Entities
 class Hair(object):
     def __init__(self):
-        self.growing = True
+        # self.growing = True # It is not used for our simulation please ignore
         self.growthCapacity = 1.0
         self.growth = 0.5
         self.growthRate = 0.1
-        self.TCell = 1.6
-        self.artery = False
+        self.TCell = 1.5
+        self.artery = False # This tells us if the hair will be introducing new T-Cells to the system.
     def checkGrowing(self):
         if self.growthRate<0.01:
             self.growing = False
         return self.growing
-    def TCellGrow(self,growthRate):
+    def TCellGrow(self, growth):
         if self.artery:
-            self.TCell+=0.2
-        self.TCell+=growthRate
+            self.TCell+=0.3
+        self.TCell+=growth
 
 # Creating the head
 class SadHair(object):
@@ -31,13 +31,13 @@ class SadHair(object):
         self.N=size
         self.hairs = empty((self.N,self.N),dtype=object)
         self.fillHairs()
-
+    # Initializes each grid with Hair object.
     def fillHairs(self):
         for j in range(self.N):
             for i in range(self.N):
                 tmp = Hair()
                 self.hairs[j,i] = tmp
-
+    # Initialize places with extra T-Cells and the sources of T-Cells
     def setUp(self):
         for j in range(self.N):
             for i in range(self.N):
@@ -48,66 +48,79 @@ class SadHair(object):
                 if j>0.3*self.N and j<0.6*self.N and (i<0.6*self.N and i>0.4*self.N):
                     self.hairs[j,i].TCell+=0.4
                     self.hairs[j,i].artery = True
-
-    def action(self):
+    # Gets T-Cells spilled from back
+    def toBack(self,j,i):
+        if j-1 >=0:
+            if self.hairs[j-1,i].TCell > 1.20:
+                increase = self.hairs[j-1,i].TCell*0.25
+                self.hairs[j-1,i].TCell-=self.hairs[j-1,i].TCell*0.25
+                return increase
+        return 0
+    # Gets T-Cells spilled from front
+    def toFront(self,j,i):
+        if j+1 < self.N:
+            if self.hairs[j+1,i].TCell > 1.20:
+                increase = self.hairs[j+1,i].TCell*0.25
+                self.hairs[j+1,i].TCell-=self.hairs[j+1,i].TCell*0.25
+                return increase
+        return 0
+    # Gets T-Cells spilled from right
+    def toRight(self,j,i):
+        if i-1 < self.N:
+            if self.hairs[j,i-1].TCell > 1.20:
+                increase = self.hairs[j,i-1].TCell*0.25
+                self.hairs[j,i-1].TCell-=self.hairs[j,i-1].TCell*0.25
+                return increase
+        return 0
+    # Gets T-Cells spilled from left
+    def toLeft(self,j,i):
+        if i+1 < self.N:
+            if self.hairs[j,i+1].TCell > 1.20:
+                increase = self.hairs[j,i+1].TCell*0.25
+                self.hairs[j,i+1].TCell-=self.hairs[j,i+1].TCell*0.25
+                return increase
+        return 0
+    # We progress in time one step in action function.
+    def action(self,t):
         for j in range(self.N):
             for i in range(self.N):
                 oneHair = self.hairs[j,i]
                 # Hair pops off when reaching max capacity
                 if oneHair.growth >=oneHair.growthCapacity:
                     oneHair.growth = 0
-                # if oneHair.checkGrowing() == True:
-                #     # Hair growth update
-                oneHair.growth += oneHair.growthRate
-                oneHair.growthRate = 0.1*oneHair.growthCapacity # Update growth rate
-                oneHair.growthCapacity = 1-1/(1+exp((-1)*oneHair.TCell+7.0)) # Update capacity
-                TCellGrowth = 0
-                # if oneHair.TCell>=10.0:
-                #     continue
-                # We need to make the balding equally possible on left and right
-                if i>= 0.5*self.N:
-                    if i+1 < self.N:
-                        if self.hairs[j,i+1].TCell > 1.00:
-                            TCellGrowth+=self.hairs[j,i+1].TCell*0.2
-                            self.hairs[j,i+1].TCell-=self.hairs[j,i+1].TCell*0.2
-                    if i-1 >=0:
-                        if self.hairs[j,i-1].TCell > 1.00:
-                            TCellGrowth+=self.hairs[j,i-1].TCell*0.2
-                            self.hairs[j,i-1].TCell-=self.hairs[j,i-1].TCell*0.2
-                elif i<= 0.5*self.N:
-                    if i-1 >=0:
-                        if self.hairs[j,i-1].TCell > 1.00:
-                            TCellGrowth+=self.hairs[j,i-1].TCell*0.2
-                            self.hairs[j,i-1].TCell-=self.hairs[j,i-1].TCell*0.2
-                    if i+1 < self.N:
-                        if self.hairs[j,i+1].TCell > 1.00:
-                            TCellGrowth+=self.hairs[j,i+1].TCell*0.2
-                            self.hairs[j,i+1].TCell-=self.hairs[j,i+1].TCell*0.2
-                # Check the front and back part of its location first.
-                if j+1 < self.N:
-                    if self.hairs[j+1,i].TCell > 1.20:
-                        TCellGrowth+=self.hairs[j+1,i].TCell*0.2
-                        self.hairs[j+1,i].TCell-=self.hairs[j+1,i].TCell*0.2
-                if j-1 >=0:
-                    if self.hairs[j-1,i].TCell > 1.20:
-                        TCellGrowth+=self.hairs[j-1,i].TCell*0.2
-                        self.hairs[j-1,i].TCell-=self.hairs[j-1,i].TCell*0.2
-                oneHair.TCellGrow(TCellGrowth)
 
+                oneHair.growth += oneHair.growthRate
+
+                oneHair.growthCapacity = 1-1/(1+exp((-1)*oneHair.TCell+4.0)) # Update capacity
+                TCellGrowth = 0
+
+                # T-Cell spills from one of the 4 directions.
+                dir = randint(4)
+                
+                if dir == 0:
+                    TCellGrowth+=self.toBack(j,i)
+                elif dir == 1:
+                    TCellGrowth+=self.toFront(j,i)
+                elif dir == 2:
+                    TCellGrowth+=self.toRight(j,i)
+                else:
+                    TCellGrowth+=self.toLeft(j,i)
+                oneHair.TCellGrow(TCellGrowth)
+    # Return T-Cell values for the grid.
     def getTCells(self):
         res = zeros((self.N,self.N),dtype=float64)
         for j in range(self.N):
             for i in range(self.N):
                 res[j,i]=self.hairs[j,i].TCell
         return res
-
+    # Return current growth values for the grid.
     def getHairs(self):
         res = zeros((self.N,self.N),dtype=float64)
         for j in range(self.N):
             for i in range(self.N):
                 res[j,i]=self.hairs[j,i].growth
         return res
-
+    # Return current growth capacities values for the grid.
     def getCapacities(self):
         res = zeros((self.N,self.N),dtype=float64)
         for j in range(self.N):
@@ -116,13 +129,14 @@ class SadHair(object):
         return res
 
 origin = 'lower'
-
-Tlevels = linspace(0.0,2.5,10)#[0.0,1.0,2.0,3.0,4.0]#,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0]
-
+# Show the levels of T-Cells
+TLevels = [0.0,0.5, 1.0,1.5,2.0,2.5,3.0]#,4.0]#,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0]
+# Tlevels = linspace(0.0,3,3)
+# Tlevels = [0.0,1.0,2.0,3.0,4.0]
 height = 20
 width = 20
-size =100
-
+size =100 # The grids will be creates with the number of size-by-size.
+# The levels of hair growth divided into 1/10
 hairLevels = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]
 
 x = linspace(0,width,size)
@@ -133,60 +147,59 @@ X,Y =meshgrid(x,y)
 
 hairs = SadHair(size)
 hairs.setUp()
+# res = hairs.getTCells()
 res = hairs.getHairs()
 
 fig1, ax2 = plt.subplots(constrained_layout=True)
 CS = ax2.contourf(X, Y, res, hairLevels, cmap=plt.cm.bone, origin=origin)
 
-# Note that in the following, we explicitly pass in a subset of
-# the contour levels used for the filled contours.  Alternatively,
-# We could pass in additional levels to provide extra resolution,
-# or leave out the levels kwarg to use all of the original levels.
+CS2 = ax2.contour(CS, hairLevels, origin=origin)
 
-CS2 = ax2.contour(CS, Tlevels, origin=origin)
-
-# ax2.set_title('T-Cell Level')
+# ax2.set_title('T-Cell Contour')
 ax2.set_title('Hair Growth')
 ax2.set_xlabel('x-direction of the head')
 ax2.set_ylabel('y-direction of the head')
 
 # Make a colorbar for the ContourSet returned by the contourf call.
 cbar = fig1.colorbar(CS)
-# cbar.ax.set_ylabel('T-Cell Level')
+# cbar.ax.set_ylabel('T-Cell Excess Level')
 cbar.ax.set_ylabel('Hair Growth')
 # Add the contour line levels to the colorbar
 cbar.add_lines(CS2)
 
-# plt.savefig("Hair_test_t=0.png")
+plt.savefig("HairGrowth_t=0.png")
 # plt.savefig("TCell_t=0.png")
 
 # For simplicity, let dt=1
-for t in range(1,201):
+for t in range(1,721):
     
-    hairs.action()
-    if t>100:
+    # number = randint(4)
+    # counter[number]+=1
+    # print("Random Number is {}".format(number))
+    hairs.action(t)
+    if t>700:
+        # res = hairs.getTCells()
         res = hairs.getHairs()
         fig1, ax2 = plt.subplots(constrained_layout=True)
         CS = ax2.contourf(X, Y, res, hairLevels, cmap=plt.cm.bone, origin=origin)
 
-        # Note that in the following, we explicitly pass in a subset of
-        # the contour levels used for the filled contours.  Alternatively,
-        # We could pass in additional levels to provide extra resolution,
-        # or leave out the levels kwarg to use all of the original levels.
-
         CS2 = ax2.contour(CS, hairLevels, origin=origin)
 
-        # ax2.set_title('T-Cell Concentration')
+        # ax2.set_title('T-Cell Level')
         ax2.set_title('Hair Growth')
         ax2.set_xlabel('x-direction of the head')
         ax2.set_ylabel('y-direction of the head')
 
         # Make a colorbar for the ContourSet returned by the contourf call.
         cbar = fig1.colorbar(CS)
-        cbar.ax.set_ylabel('T-Cell Concentration')
+        # cbar.ax.set_ylabel('T-Cell Concentration')
         cbar.ax.set_ylabel('Hair Growth')
         # Add the contour line levels to the colorbar
         cbar.add_lines(CS2)
 
-        plt.savefig("Hair_test_t={}.png".format(t))
+        plt.savefig("HairGrowth_t={}.png".format(t))
+        # plt.savefig("TCell_t={}.png".format(t))
+        del(fig1)
+        del(ax2)
+        plt.close('all')
 # plt.show()
